bd00512ca5fc1449589caf42869e4e9a6817714c
Akshar-2011 <84450951+Akshar-2011@users.noreply.github.com>
2022-02-18 18:42:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var sound = "";
 var sound1 = "";
+leftWristX = "";
+leftWristY = "";
+rightWristX = "";
+rightWristY = "";
 function preload(){
     sound = loadSound(music.mp3);
     sound1 = loadSound(music2.mp3);
@@ -9,7 +13,20 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    posenet = ml5.poseNet(video, modalLoaded);
+    posenet.on('pose', gotPoses);
+}
+function modalLoaded(){
+    console.log("Posenet is initialized");
 }
 function draw(){
     image(video, 0, 0, 500, 400);
+}
+function gotPoses(results){
+    if(results > 0){
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+    }
 }
